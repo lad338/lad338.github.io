4206bea44fcae516b74c8f21707a585c2fdff3d8
Dicky Li <dickyli338@gmail.com>
2023-03-10 12:44:31 -0500

    update resume text to be center align

--- src/features/resume/ResumeText.tsx
+++ src/features/resume/ResumeText.tsx
@@ -5,11 +5,23 @@ import { Link } from '@mui/material'
 
 export const ResumeText: React.FC = () => {
   return (
-    <Box id="resume-text" margin="auto" width="84vw" marginBottom={5}>
-      <Typography variant="h4" marginBottom={5} color="text.secondary">
+    <Box
+      id="resume-text"
+      margin="auto"
+      width="84vw"
+      marginBottom={5}
+      display="flex"
+      flexDirection="column"
+    >
+      <Typography
+        variant="h4"
+        marginBottom={5}
+        color="text.secondary"
+        textAlign="center"
+      >
         Resume
       </Typography>
-      <Typography color="grey.500">
+      <Typography color="grey.500" textAlign="center">
         You may also download my latest resume{' '}
         <Link
           href="/angus-resume-20230309.pdf"
